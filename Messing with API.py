2124bec9a6dce0e4463545767f25fd5b3008523a
ammarwins2 <ammar@mooraj.com>
2017-08-16 03:11:37 +0400

    Week 1 work, could ass more, and I may but this is the bare bones of what i was asked to do. I think

---
+++ Messing with API.py
@@ -0,0 +1,17 @@
+#Messing around with API
+import requests
+#this program should show give champ info based on id input
+
+#function to provide json file
+def championinfo(cID, uAPIkey):
+    info = requests.get("https://euw1.api.riotgames.com/lol/platform/v3/champions/"+ cID +"/?api_key="+uAPIkey)
+    return info.json()
+
+def f2p(info):
+    print [info][0]['freeToPlay']
+#required inputs for program to run    
+cID = (str)(raw_input("enter a champ id number"))
+uAPIkey =(str) (raw_input ("enter your api key"))
+print championinfo(cID, uAPIkey)
+info = championinfo(cID, uAPIkey)
+f2p(info)
