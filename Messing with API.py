32efec5b0f0620ad4e4d5cfa4b3d10f6f751f326
ammarwins2 <ammar@mooraj.com>
2017-08-20 17:45:51 -0400

    I had time to kill so i decided to see if i could make the program more easily user friendly incase in the future I work on it enough to actually be useful for someone, the foundation of a userfriendly program are set.

--- Messing with API.py
+++ Messing with API.py
@@ -1,17 +1,37 @@
 #Messing around with API
 import requests
+import sys
 #this program should show give champ info based on id input
 
 #function to provide json file
-def championinfo(cID, uAPIkey):
+def champinfo(cID, uAPIkey):
     info = requests.get("https://euw1.api.riotgames.com/lol/platform/v3/champions/"+ cID +"/?api_key="+uAPIkey)
     return info.json()
 #function to check if the champ is free to play based on the chamions id
+free = (str)
 def f2p(info):
     print [info][0]['freeToPlay']
 #required inputs for program to run    
 cID = (str)(raw_input("enter a champ id number"))
 uAPIkey =(str) (raw_input ("enter your api key"))
-print championinfo(cID, uAPIkey)
-info = championinfo(cID, uAPIkey)
-f2p(info)
+
+#Below I make a while statement that makes the program more user friendly incase it ever expands into somthing useful and distributable as opposed to just an excercise
+#I also had some time to kill
+while True:
+    func = (str)(raw_input("what would you like me to do? (type help for commands)"))
+    if func == "champinfo":
+        print champinfo(cID, uAPIkey)
+    elif func == "f2p":
+        info = champinfo(cID, uAPIkey)
+        f2p(info)
+    elif func == "help":
+        print "type"
+        print "champinfo for champion informations"
+        print "f2p to find out if the champion is free to play"
+        print "exit to exit"
+    elif func == "exit":
+        break
+    else:
+        print "enter a valid command"
+
+sys.exit()
